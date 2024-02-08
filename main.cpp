2b7035f60c7ef568d091231a8ffb0972660c1ffe
Stefan Holmberg <stefan.holmberg@systementor.se>
2024-02-08 09:14:05 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+int main(){
+    int i = 11;
+    int j = 12;
+    int k = 13;
+
+    int b = findBiggest(i,j,k);
+}
